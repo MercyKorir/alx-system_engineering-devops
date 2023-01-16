599302d9442b8810f69f8818148dded3204ec7d5
MercyKorir <mcthegreat78@gmail.com>
2023-01-16 12:14:41 +0300

    I have added the file for 0*

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,20 +7,15 @@ import json
 import sys
 import urllib.request
 
-
-def get_todo_list(employee_id):
-    # Makes a GET request to API and retrieves employee info
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
     url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
     response = urllib.request.urlopen(url)
     data = json.loads(response.read())
-
-    # Get the employee's name
     name_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
     name_response = urllib.request.urlopen(name_url)
     name_data = json.loads(name_response.read())
     employee_name = name_data["name"]
-
-    # Count the number of completed tasks
     completed_tasks = 0
     total_tasks = len(data)
     completed_task_title = []
@@ -28,14 +23,7 @@ def get_todo_list(employee_id):
         if task["completed"] is True:
             completed_tasks += 1
             completed_task_title.append(task["title"])
-
-    # Print the results
     print("Employee " + employee_name + " is done with " +
           "tasks(" + str(completed_tasks) + "/" + str(total_tasks) + "):")
     for task in completed_task_title:
         print("\t" + task)
-
-
-if __name__ == "__main__":
-    employee_id = int(sys.argv[1])
-    get_todo_list(employee_id)
