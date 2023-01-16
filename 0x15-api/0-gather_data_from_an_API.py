613663c33918db6ed76fe3cc3772a71a4190132f
MercyKorir <mcthegreat78@gmail.com>
2023-01-16 13:52:04 +0300

    I have added the file 0*

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
-"""Returns information about todo list progress for given employee ID."""
+"""Returns info about to-do list progress for given employee ID"""
 import json
+import requests
 import sys
-import urllib.request
-
 
 if __name__ == "__main__":
     employee_id = int(sys.argv[1])
-    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
             employee_id)
-    response = urllib.request.urlopen(url)
-    data = json.loads(response.read())
     name_url = "https://jsonplaceholder.typicode.com/users/{}".format(
             employee_id)
-    name_response = urllib.request.urlopen(name_url)
-    name_data = json.loads(name_response.read())
-    employee_name = name_data.get("name")
+    response = requests.get(todos_url)
+    todos = json.loads(response.text)
+    names = requests.get(name_url)
+    name_data = json.loads(names.text)
     completed_tasks = 0
-    total_tasks = len(data)
-    completed_task_title = []
-    for task in data:
-        if task.get("completed") is True:
+    for todo in todos:
+        if todo.get("completed") is True:
             completed_tasks += 1
-            completed_task_title.append(task.get("title"))
-    print("Employee " + employee_name + " is done with " +
-          "tasks(" + str(completed_tasks) + "/" + str(total_tasks) + "):")
-    for task in completed_task_title:
-        print("\t" + task)
+    print("Employee {} is done with tasks({}/{}):".format(
+        name_data.get("name"), completed_tasks, len(todos)))
+    for todo in todos:
+        if todo.get("completed") is True:
+            print('\t{}'.format(todo.get("title")))
---
+++ 0x15-api/other-0.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Returns information about todo list progress for given employee ID."""
+import json
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+            employee_id)
+    response = urllib.request.urlopen(url)
+    data = json.loads(response.read())
+    name_url = "https://jsonplaceholder.typicode.com/users/{}".format(
+            employee_id)
+    name_response = urllib.request.urlopen(name_url)
+    name_data = json.loads(name_response.read())
+    employee_name = name_data.get("name")
+    completed_tasks = 0
+    total_tasks = len(data)
+    completed_task_title = []
+    for task in data:
+        if task.get("completed") is True:
+            completed_tasks += 1
+            completed_task_title.append(task.get("title"))
+    print("Employee " + employee_name + " is done with " +
+          "tasks(" + str(completed_tasks) + "/" + str(total_tasks) + "):")
+    for task in completed_task_title:
+        print("\t" + task)
