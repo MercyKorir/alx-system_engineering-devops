0e5f919eeb033cdac1b740d2c21b299c70efa3de
MercyKorir <mcthegreat78@gmail.com>
2023-01-16 14:29:42 +0300

    i have added the file 1*

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,52 +1,24 @@
 #!/usr/bin/python3
-"""This script uses REST API for given employee ID and returns
-information about todo list progress
-"""
+"""Returns info about to-do list progress for given employee ID"""
 import csv
 import json
+import requests
 import sys
-import urllib.request
-
-
-def get_todo_list(employee_id):
-    """Makes a GET request to API and retrieves employee info"""
-    url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-    response = urllib.request.urlopen(url)
-    data = json.loads(response.read())
-
-    # Get the employee's name
-    name_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    name_response = urllib.request.urlopen(name_url)
-    name_data = json.loads(name_response.read())
-    employee_name = name_data["name"]
-
-    # Count the number of completed tasks
-    completed_tasks = 0
-    total_tasks = len(data)
-    completed_task_title = []
-    for task in data:
-        if task["completed"] is True:
-            completed_tasks += 1
-            completed_task_title.append(task["title"])
-
-    # Print the results
-    print("Employee " + employee_name + " is done with " +
-          "tasks(" + str(completed_tasks) + "/" + str(total_tasks) + "):")
-    for task in completed_task_title:
-        print("\t" + task)
-
-    csv_data = [['userId', 'userName', 'completed', 'title']]
-    for item in data:
-        csv_data.append([item['userId'], employee_name, item['completed'],
-                        item['title']])
-
-    filename = "{}.csv".format(str(sys.argv[1]))
-
-    with open(filename, 'w', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerows(csv_data)
-
 
 if __name__ == "__main__":
     employee_id = int(sys.argv[1])
-    get_todo_list(employee_id)
+    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+            employee_id)
+    name_url = "https://jsonplaceholder.typicode.com/users/{}".format(
+            employee_id)
+    response = requests.get(todos_url)
+    todos = json.loads(response.text)
+    names = requests.get(name_url)
+    name_data = json.loads(names.text)
+    csv_data = [[]]
+    for item in todos:
+        csv_data.append([item.get("userId"), name_data.get("username"), item.get("completed"), item.get("title")])
+    file_name = "{}.csv".format(str(sys.argv[1]))
+    with open(file_name, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        csvwriter.writerows(csv_data)
