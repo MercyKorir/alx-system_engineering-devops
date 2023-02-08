226061ddf99742b9f9a2d39faa7304b3d0ff8f34
MercyKorir <mcthegreat78@gmail.com>
2023-02-08 09:45:24 +0300

    I have added the file for task 0*

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# increases allowed worker connections
+
+exec { 'fix-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx
+',
+  path    => 'usr/local/bin/:/bin/'
+} ->
+
+exec { 'restart-nginx':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
